226eb9d08a6bc0c3eba002acb66ff6d63f4d5178
jelliotartz <jelliotartz@gmail.com>
2015-10-21 21:49:19 -0400

    finish 4.5 exercises

--- week-4/leap-years/my_solution.rb
+++ week-4/leap-years/my_solution.rb
@@ -1,6 +1,18 @@
 # Leap Years
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge Natasha
 
 
 # Your Solution Below
+
+def leap_year?(year)
+  if year % 400 != 0 && year % 100 == 0
+    p false
+  elsif year % 4 == 0
+    p true
+  elsif year <= 3
+    p false
+  else
+    p false
+  end
+end
--- week-4/triangle-side/my_solution.rb
+++ week-4/triangle-side/my_solution.rb
@@ -9,7 +9,7 @@ def valid_triangle?(a, b, c)
     a == 0 or b == 0 or c == 0
     p false
   elsif
-    a + b>= c and a + c>= b and b + c >= a
+    a + b>= c and a + c >= b and b + c >= a
     p true
   else
     p false
