df7c5e57e0d8f1f9cc486eac0b5f2125718bad19
jelliotartz <jelliotartz@gmail.com>
2015-10-28 19:43:37 -0400

    edit calc mode

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -13,24 +13,6 @@
 
 # 0. Pseudocode
 
-# What is the input? What is the output? (i.e. What should the code
-# return?) What are the steps needed to solve the problem?
-
-
-# Calculate the mode Pairing Challenge
-
-# I worked on this challenge with: Jon Clayton
-
-# I spent 2 hours on this challenge.
-
-# Complete each step below according to the challenge directions and
-# include it in this file. Also make sure everything that isn't code
-# is commented.
-
-
-
-# 0. Pseudocode
-
 
 # What is the input?
 
