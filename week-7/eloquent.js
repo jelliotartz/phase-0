b9980389e2d56d5197665d09094b190def918d9e
jelliotartz <jelliotartz@gmail.com>
2015-11-11 10:43:29 -0500

    add to eloquent.js

--- week-7/eloquent.js
+++ week-7/eloquent.js
@@ -13,8 +13,8 @@ console.log(myFirstVariable + myNextVariable);
 
 // chapter 2, interacting with the Chrome console:
 
-prompt("what's your favorite food?");
-console.log("Hey! That's my favorite food too!");
+// prompt("what's your favorite food?");
+// console.log("Hey! That's my favorite food too!");
 
 
 
@@ -100,17 +100,17 @@ Reflections on Chess Board exercise:
 
 full disclosure: I puzzled over this for a while, then looked at the
 solution. As I look at the solution, I have a hard time following the
-logic regarding how the board accurately reads where to insert the \n.
-Or, for that matter, how it knows when to insert a #. It seems like
-whenever y increases, x must increase as well... but this can't be
-true, because then x + y would always be even, and thus x + y % 2
-would always == 0. I guess there's something about the for loop within
-the for loop that I'm not understanding yet, that allows both of these
-things (insert # after space, and put the new line at the correct
-position) to happen.
+logic regarding how the board accurately reads where to insert the
+newline (\n). Or, for that matter, how it knows when to insert a #. It
+seems like whenever y increases, x must increase as well... but this
+can't be true, because then x + y would always be even, and thus x + y
+% 2 would always == 0. I guess there's something about the for loop
+within the for loop that I'm not understanding yet, that allows both
+of these things (insert # after space, and put the new line at the
+correct position) to happen.
+
 
 */
-
 
 // Functions
 
@@ -131,3 +131,14 @@ console.log(min(0, 10));
 // Data Structures: Objects and Arrays
 
 // Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+
+var me = {
+  name: "James Artz",
+  age: 37,
+  favoriteFoods: ["Pasta Marinara", "Lobster", "Blueberries"],
+  quirk: "My cats are immigrants"
+};
+
+
+
+console.log(me);
