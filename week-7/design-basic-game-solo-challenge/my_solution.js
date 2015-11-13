98ca19ddb13a00bc0a4ff9fc704eb14d49be1989
jelliotartz <jelliotartz@gmail.com>
2015-11-13 17:37:34 -0500

    work on nums-commas

--- week-7/design-basic-game-solo-challenge/my_solution.js
+++ week-7/design-basic-game-solo-challenge/my_solution.js
@@ -3,23 +3,57 @@
 // This is a solo challenge
 
 // Your mission description:
-// Overall mission:
-// Goals:
-// Characters:
-// Objects:
-// Functions:
+// Overall mission: Secure the Ark of the Covenant
+// Goals: Find the Ark, avoid the Nazi.
+// Characters: Indiana Jones, Nazi
+// Objects: Ark of the Covenant.
+// Functions: moveUp, moveDown, moveForward, moveBack
 
-// Pseudocode
-//
-//
-//
-//
-//
+/*
+Pseudocode
+
+Create a player, "Indiana Jones," that will be able to move around a coordinate plane (up, down, forward, back).
+
+Create four methods that will execute movement in these four directions.
+
+Create a "Nazi" object and give it a random position.
+
+Create an "Ark of the Covenant" object and give it a random position. This position will be fixed throughout the duration of the game.
+
+Move Indiana Jones, and randomly reassign the position of the Nazi.
+
+Check where Indiana Jones is in relation to the Nazi and the Ark.
+
+Accept input for next move of Indiana Jones, UNTIL:
+
+  If Indiana Jones moves and his grid coordinates are the same as the Ark, success!
+
+    Output pithy "you won the game" message. END.
+
+  If Indiana Jones moves and his grid coordinates are the same as the Nazi's, failure!
+
+    Output pithy "you lost the game" message. END.
+
+*/
 
 // Initial Code
 
+var indianaJones = {
+  posX: 0,
+  posY: 0,
+  ark: 0,
+  success: false,
 
+  move:function(direction) {
 
+  }
+}
+
+var arkOfTheCovenant = {
+  posX = function() {
+
+  }
+}
 
 
 
