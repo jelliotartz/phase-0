3f0bc76d6028e28093e9fe082eba7f2f5526faa0
jelliotartz <jelliotartz@gmail.com>
2015-11-12 23:49:08 -0500

    edit js olympics

--- week-7/design-basic-game-solo-challenge/my_solution.js
+++ week-7/design-basic-game-solo-challenge/my_solution.js
@@ -39,3 +39,30 @@
 //
 //
 //
+
+
+/*
+
+var homer = {
+  posX: 0,
+  posY: 0
+}
+var beer = {
+  posX: function() {
+    // gereate random coordat
+  }
+}
+var bar = {}
+
+
+
+
+function move(x, y) {
+
+}
+
+function update() {
+
+}
+
+*/
